0day-ci/dpdk

vhost: fix null pointer dereference
Return value of function get_device() is not checking before
dereference. Fix this problem by adding checking condition.

Coverity issue: 119262
Fixes: 77d20126b4c2 ("vhost-user: handle message to enable vring")

Signed-off-by: Marcin Kerlin <marcinx.kerlin@intel.com>

lib/librte_vhost/vhost_user/virtio-net-user.c
@@ -332,8 +332,12 @@ int
 user_set_vring_enable(struct vhost_device_ctx ctx,
 		      struct vhost_vring_state *state)
 {
-	struct virtio_net *dev = get_device(ctx);
+	struct virtio_net *dev;
 	int enable = (int)state->num;
+
+	dev = get_device(ctx);
+	if (dev == NULL)
+		return -1;
 
 	RTE_LOG(INFO, VHOST_CONFIG,
 		"set queue enable: %d to qp idx: %d\n",

damirsamardzic/dpdk-marvell

Acked-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>

lib/librte_vhost/vhost_user/virtio-net-user.c
@@ -335,8 +335,12 @@ user_get_vring_base(int vid, struct vhost_vring_state *state)
 int
 user_set_vring_enable(int vid, struct vhost_vring_state *state)
 {
-	struct virtio_net *dev = get_device(vid);
+	struct virtio_net *dev;
 	int enable = (int)state->num;
+
+	dev = get_device(vid);
+	if (dev == NULL)
+		return -1;
 
 	RTE_LOG(INFO, VHOST_CONFIG,
 		"set queue enable: %d to qp idx: %d\n",